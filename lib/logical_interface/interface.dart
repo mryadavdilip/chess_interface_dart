0993f73694fbbffaa376dabac726e0aac672183b
mryadavdilip <mryadavdilip@gmail.com>
2025-04-17 17:16:21 +0530

    utils

--- lib/logical_interface/interface.dart
+++ lib/logical_interface/interface.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'move_validator.dart';
 import 'piece.dart';
@@ -9,6 +10,33 @@ class Position {
   final int col;
 
   Position({required this.row, required this.col});
+
+  /// Either [String] or [Map<String, int>]
+  factory Position.fromJson(dynamic json) {
+    assert(json != null, 'JSON cannot be null');
+    assert(
+      json is String || json is Map<String, int>,
+      'Invalid JSON format for Position',
+    );
+    json = (json is String ? jsonDecode(json) : json) as Map<String, int>;
+
+    assert(
+      json['row'] != null && json['col'] != null,
+      'row or col cannot be null in JSON format for Position',
+    );
+    return Position(row: json['row']!, col: json['col']!);
+  }
+
+  /// Either [String] or [Map<String, int>]
+  T toJson<T>() {
+    Map<String, int> map = {'row': row, 'col': col};
+
+    if (T == String) {
+      return jsonEncode(map) as T;
+    } else {
+      return map as T;
+    }
+  }
 
   @override
   bool operator ==(Object other) =>
--- lib/logical_interface/piece.dart
+++ lib/logical_interface/piece.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /// Chess pieces
 enum PieceType { pawn, knight, bishop, rook, queen, king }
 
@@ -10,4 +12,34 @@ class ChessPiece {
   final PieceColor color;
 
   ChessPiece({required this.type, required this.color});
+
+  /// Either [String] or [Map<String, dynamic>]
+  factory ChessPiece.fromJson(dynamic json) {
+    assert(json != null, 'JSON cannot be null');
+    assert(
+      json is String || json is Map<String, dynamic>,
+      'Invalid JSON format for ChessPiece',
+    );
+    json = (json is String ? jsonDecode(json) : json) as Map<String, dynamic>;
+
+    assert(
+      json['type'] != null && json['color'] != null,
+      'type or color cannot be null in JSON format for ChessPiece',
+    );
+    return ChessPiece(
+      type: PieceType.values.byName(json['type']),
+      color: PieceColor.values.byName(json['color']),
+    );
+  }
+
+  /// Either [String] or [Map<String, dynamic>]
+  T toJson<T>() {
+    Map<String, dynamic> map = {'type': type.name, 'color': color.name};
+
+    if (T == String) {
+      return jsonEncode(map) as T;
+    } else {
+      return map as T;
+    }
+  }
 }
